9674308570c893c17ebf9f7045617e8f4813269b
Vladislav Kozlenko <vladislavko@softwareplanet.uk.com>
2018-08-01 10:45:04 +0300

    first

--- models/Guest.js
+++ models/Guest.js
@@ -4,7 +4,13 @@ const GuestSchema = new mongoose.Schema({
   lastName: {type: String },
   createdAt: {type: Date, default: Date.now },
     password: {type:String, required: true},
-    email: {type: String, required: true}
+    email: {type: String, required: true},
+    skills:[
+      {
+        _id: {type: ObjectId},
+        name:{type: String}
+      }
+    ]
 });
 
 const Guest = mongoose.model('Guest', GuestSchema, 'guests');
